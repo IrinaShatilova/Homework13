244c200690ddee62180065c8100d45fe6324ad01
shati <shatilova_ira@mail.ru>
2022-12-17 01:53:34 +0600

    ДЗ 13 ВЫПОЛНЕНО

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Homework 13");
+        System.out.println("Homework 13");Author levTolstoy = new Author("Лев"," Толстой");
+        Book warAndWorld = new Book("Война и мир",levTolstoy,1980);
+        Book warAndWorld1 = new Book("Война и мир",levTolstoy,1981);
+        Author aleksandrPushkin = new Author("Александр"," Пушкин");
+        Book evgeniyOnegin = new Book("Евгений Онегин", aleksandrPushkin,1979);
+        evgeniyOnegin.setYear(2022);
+        System.out.println("Книга 1: " + warAndWorld);
+        System.out.println("Книга 2: " + evgeniyOnegin);
+        System.out.println("Книга 3: " + warAndWorld1);
+        if (warAndWorld.equals(warAndWorld1)) {
+            System.out.println("Такая книга уже есть в библиотеке");
+        }
+
     }
 }
